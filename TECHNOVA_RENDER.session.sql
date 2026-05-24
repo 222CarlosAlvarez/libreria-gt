e72022081ad165b6a54308a8f1fb71e924f628f9
Carlos Alvarez <222carlosalvarez@gmail.com>
2026-05-24 02:10:26 -0600

    fix import excel

--- TECHNOVA_RENDER.session.sql
+++ TECHNOVA_RENDER.session.sql
@@ -22,3 +22,8 @@ VALUES (
     'fecha_creacion:timestamp without time zone',
     'fecha_actualizacion:timestamp without time zone'
   );
+<<<<<<< HEAD
+=======
+
+ALTER TABLE productos ADD COLUMN sku TEXT UNIQUE;
+>>>>>>> 47767a3 (fix import excel)
